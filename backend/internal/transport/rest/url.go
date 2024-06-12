5424f939c2539edf1da62b8fb209fe07f3764485
JustNik <nikita-999999@yandex.ru>
2024-06-12 23:16:34 +0300

    fix cors

--- backend/internal/transport/rest/url.go
+++ backend/internal/transport/rest/url.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"log"
 	"log/slog"
 	"net/http"
 
@@ -97,7 +98,8 @@ func (h *URLHandler) FollowUrl(w http.ResponseWriter, r *http.Request) {
 //	@Failure		500		{object}	response.Body
 //	@Router			/api/save_url [post]
 func (h *URLHandler) SaveURL(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Access-Control-Allow-Origin", r.Host)
+	log.Println(r.Host)
+	w.Header().Add("Access-Control-Allow-Origin", "http://158.160.38.38")
 	w.Header().Add("Access-Control-Allow-Credentials", "true")
 
 	var longURLData dto.LongURLData
