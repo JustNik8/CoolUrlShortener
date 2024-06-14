3dde89c3c29e9bfe3746c500be1a8cd7bb21d964
JustNik <nikita-999999@yandex.ru>
2024-06-14 16:55:42 +0300

    remove http from analytics service

--- analytics_service/internal/transport/rest/dto/pagination.go
+++
@@ -1,9 +0,0 @@
-package dto
-
-type Pagination struct {
-	Next          int `json:"next"`
-	Previous      int `json:"previous"`
-	RecordPerPage int `json:"record_per_page"`
-	CurrentPage   int `json:"current_page"`
-	TotalPage     int `json:"total_page"`
-}
