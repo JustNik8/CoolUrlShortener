6a6a164fcb8e4f89d72befc0f13d2281587c3a55
JustNik <nikita-999999@yandex.ru>
2024-06-16 15:27:31 +0300

    add tests for analytivs service

--- analytics_service/internal/repository/analytics.go
+++ analytics_service/internal/repository/analytics.go
@@ -6,6 +6,7 @@ import (
 	"analytics_service/internal/domain"
 )
 
+//go:generate go run github.com/vektra/mockery/v2@v2.42.1 --name AnalyticsRepo
 type AnalyticsRepo interface {
 	GetTopUrls(ctx context.Context, paginationParams domain.PaginationParams) ([]domain.TopURLData, error)
 }
